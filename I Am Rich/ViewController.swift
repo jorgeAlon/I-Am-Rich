afab5e93daea27929cc89bf0763fdfc34aa9e0ff
jorgeAlon <alonsojorget@gmail.com>
2020-02-19 22:41:45 -0600

    Primera app con IOS

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Jorge Alonso on 07/01/20.
+//  Copyright © 2020 Jorge Alonso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
